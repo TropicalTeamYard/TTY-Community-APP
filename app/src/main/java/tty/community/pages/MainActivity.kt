1807b3151605f81b4b3a880ed49edf86a35c3669
wcf <1533144693@qq.com>
2019-07-08 14:31:08 +0800

    create project

---
+++ app/src/main/java/tty/community/pages/MainActivity.kt
@@ -0,0 +1,14 @@
+package tty.community.pages
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import tty.community.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
